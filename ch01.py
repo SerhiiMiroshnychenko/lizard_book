0e9b73d3c5fdb593c4ad6c6d3eb28aef759c7bbf
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-11-09 13:53:30 +0200

    Initial commit

---
+++ ch01.py
@@ -0,0 +1,21 @@
+"""
+Example 1-1. A deck as a sequence of playing cards
+"""
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
