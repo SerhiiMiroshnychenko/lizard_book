9e210ef1d67b3ea0f48835cacc967b35397b1258
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-11-13 12:55:17 +0200

    Ch02 updated

--- ch02.py
+++ ch02.py
@@ -25,3 +25,12 @@ print(f'{beyond_ascii = }')
 
 beyond_ascii = list(filter(lambda c: c > 127, map(ord, symbols)))
 print(f'{beyond_ascii = }')
+
+"""
+Example 2-4. Cartesian product using a list comprehension
+"""
+
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+tshirts = [(color, size) for color in colors for size in sizes]
+print(f'{tshirts = }')
