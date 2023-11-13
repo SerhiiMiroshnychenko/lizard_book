c7bd8bdb588c7a9b60be6047d50bfdd4c39f910f
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-11-13 09:36:57 +0200

    Update ch01.py

--- ch01.py
+++ ch01.py
@@ -56,3 +56,32 @@ def spades_high(card):
 
 for card in sorted(deck, key=spades_high):
     print(card)
+
+"""
+Example 1-2. A simple two-dimensional vector class
+"""
+
+import math
+
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
