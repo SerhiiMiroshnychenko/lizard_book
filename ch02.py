e18a554ccaffc82375b9b98fbea6bbfbb9f7545b
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-11-13 12:50:05 +0200

    Ch02 updated

--- ch02.py
+++ ch02.py
@@ -1,8 +1,13 @@
 """
-Example 2-1. Build a list of Unicode codepoints from a string
+Example 2-1. Build a list of Unicode codepoints from a string, using a listcomp
 """
 
 
 symbols = '$¢£¥€¤'
 codes = [ord(symbol) for symbol in symbols]
 print(f'{codes=}')
+
+x = 'ABC'
+codes = [ord(x) for x in x]
+print(f'{x = }')
+print(f'{codes = }')
