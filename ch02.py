04322255084be536af316465ae4dd5ab8db89923
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-11-23 10:15:18 +0200

    Update ch02.py

--- ch02.py
+++ ch02.py
@@ -227,3 +227,16 @@ if __name__ == '__main__':
 """
 Example 2-9. Method from an imaginary Robot class
 """
+
+# def handle_command(self, message):
+#     match message:
+#         case ['BEEPER', frequency, times]:
+#             self.beep(times, frequency)
+#         case ['NECK', angle]:
+#             self.rotate_neck(angle)
+#         case ['LED', ident, intensity]:
+#             self.leds[ident].set_brightness(ident, intensity)
+#         case ['LED', ident, red, green, blue]:
+#             self.leds[ident].set_color(ident, red, green, blue)
+#         case _:
+#             raise InvalidCommand(message)
